fbd08aa02cd94d946c3b2bf75b045dc15cb00fd4
Oushim959 <4312239@upm.edu.sa>
2025-07-23 11:37:38 +0300

    Yahhh, it's rewind time

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GIT_TEST
+
+Test, this is a test. test test this is a test
---
+++ TESTING.py
@@ -0,0 +1,9 @@
+print ("Yeaahhh!! it's rewind time!!")
+
+
+for i in range(11):
+
+    if i%2 == 0:
+        print("Ahhhh that's hot, that's hot")
+    else:
+        print("i wanna watch some fortite and Markass Brownlee ")    
